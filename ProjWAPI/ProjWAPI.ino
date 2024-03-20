28c4afbfaf7d43eb8cbc6e084089b320248eb099
Sw4rtz <4aronswartz@gmail.com>
2024-03-20 20:47:49 +0000

    Hora + Limpeza
    Changelog:
    - Limpeza geral do código e melhoramento de comentários.
    
    - Adição da gestão da data e hora atual de leitura dos sensores e mostragem no Serial Monitor

--- ProjWAPI/ProjWAPI.ino
+++ ProjWAPI/ProjWAPI.ino
@@ -4,9 +4,8 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <TimeLib.h>
-#include "DHT20.h"
-#include "esp_secrets.h"
 
+#include "esp_secrets.h"
 
 // Configuração da rede Wi-Fi
 const char* ssid = SECRET_SSID;
@@ -16,21 +15,17 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 WiFiUDP ntpUDP;
-  NTPClient timeClient(ntpUDP);
-
+NTPClient timeClient(ntpUDP);
 
 DHT20 DHT;
 
-uint8_t count = 0;
-const int ldrPin = 34;       // pin 34
-int sensorValue = 0;
-const int ledPin = 2;
+const int ldrPin = 34; // Pino do LDR
+const int ledPin = 2;  // Pino do LED
+int sensorValue = 0;   // Variável para armazenar o valor do LDR
 
-// Variável de controle do estado do LED para o MQTT
-// bool ledState = false;
+String formattedDate; // Hora formatada
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   // Inicialização da conexão Wi-Fi
@@ -39,95 +34,85 @@ void setup()
     delay(1000);
     Serial.println("A conectar ao WiFi...");
   }
-  
-      Serial.println("");
-      Serial.println("Ligação ao WiFi estabelecida.");
+  Serial.println("Ligação ao WiFi estabelecida.");
 
+  pinMode(ldrPin, INPUT);    // Define ldrPin como saída
+  pinMode(ledPin, OUTPUT);   // Define o pino do LED como saída
 
-
-  pinMode(ldrPin, OUTPUT);    // Define ldrPin como saída
-  Wire.begin();
-  DHT.begin();    //  ESP32 default pins 21 22
-
-  // Define o pino do led como saída
-  pinMode(ledPin, OUTPUT);
-
-  delay(1000);
+  Wire.begin();   // Ativa o protocolo I2C, necessário para o sensor DHT20
+  DHT.begin();    // ESP32 default pins 21 22
 }
 
 void loop() {
+  // Obtem a hora atual formatada
+  timeClient.update();
+  formattedDate = getCurrentTimeString();  
 
+  Serial.print("\nHora: ");
+  Serial.print(formattedDate);
 
-  // Status do led para o MQTT
-    //int ledStatus = digitalRead(ledPin);
+  // Ler dados dos sensores (Remover provoca a não leitura do DHT20)
+  uint32_t start = micros();
+  int status = DHT.read();
+  uint32_t stop = micros();
 
   // Leitura do sensor DHT20
-  if (millis() - DHT.lastRead() >= 1000)
-  {
-    //  Ler dados dos sensores
-    uint32_t start = micros();
-    int status = DHT.read();
-    uint32_t stop = micros();
+  float humidity = DHT.getHumidity();
+  float temperature = DHT.getTemperature();
 
-    if ((count % 10) == 0)
-    {
-      count = 0;
-      Serial.println();
-      Serial.println("Type\tHumidity (%)\tTemp (°C)\tTime (µs)\tStatus");
-    }
-    count++;
-
-    Serial.print("DHT20 \t");
-    //  DISPLAY DATA, sensor has only one decimal.
-    Serial.print(DHT.getHumidity(), 1);
-    Serial.print("\t\t");
-    Serial.print(DHT.getTemperature(), 1);
-    Serial.print("\t\t");
-    Serial.print(stop - start);
-    Serial.print("\t\t");
-    switch (status)
-    {
-      case DHT20_OK:
-        Serial.print("OK");
-        break;
-      case DHT20_ERROR_CHECKSUM:
-        Serial.print("Checksum error");
-        break;
-      case DHT20_ERROR_CONNECT:
-        Serial.print("Connect error");
-        break;
-      case DHT20_MISSING_BYTES:
-        Serial.print("Missing bytes");
-        break;
-      case DHT20_ERROR_BYTES_ALL_ZERO:
-        Serial.print("All bytes read zero");
-        break;
-      case DHT20_ERROR_READ_TIMEOUT:
-        Serial.print("Read time out");
-        break;
-      case DHT20_ERROR_LASTREAD:
-        Serial.print("Error read too fast");
-        break;
-      default:
-        Serial.print("Unknown error");
-        break;
-    }
-    Serial.print("\n");
-  }
-
-  sensorValue = analogRead(ldrPin);    // Lê o valor do sensor LDR e armazena na variável sensorValue
+  Serial.print("\nHumidade: ");
+  Serial.print(humidity, 1);
+  Serial.print(" %\n");
+  Serial.print("Temperatura: ");
+  Serial.print(temperature, 1);
+  Serial.println("°C\n");
+  
+// Leitura do sensor LDR
+  sensorValue = analogRead(ldrPin);
   Serial.print("Valor lido pelo LDR: ");
   Serial.println(sensorValue);
-  
+
+  // Verifica o valor do LDR e ajusta o LED
   if (sensorValue < 2600) {
-    Serial.println("Dia");    // Se o valor do LDR for menor que 2600, considera-se que está de dia
-    digitalWrite(ledPin, LOW); // Apaga o led
-  } else if (sensorValue >= 2500 && sensorValue < 3000) {
-    Serial.println("Nublado/Nascer ou pôr do sol");    // Se o valor do LDR estiver entre 2500 e 3000, considera-se que está nublado ou no nascer ou pôr do sol
+    Serial.println("Dia");
+    digitalWrite(ledPin, LOW); // Apaga o LED durante o dia
   } else {
-    Serial.println("Noite");    // Se o valor do LDR for maior ou igual a 3000, considera-se que está de noite (O sensor vai de 0 a 4095)
-    digitalWrite(ledPin, HIGH); // Acende o led
+    Serial.println("Noite");
+    digitalWrite(ledPin, HIGH); // Acende o LED durante a noite
   }
-  
-  delay(10000);
+  delay(10000); // Delay entre leituras
+}
+
+String getCurrentTimeString() {
+  // Recebe a hora atual
+  time_t currentTime = timeClient.getEpochTime();
+
+  // Recebe os valores da hora atual separadamente
+  int currentHour = hour(currentTime);
+  int currentMinute = minute(currentTime);
+  int currentSecond = second(currentTime);
+
+  // Recebe a data atual
+  int currentDay = day(currentTime);
+  int currentWeekday = weekday(currentTime);
+  int currentMonth = month(currentTime);
+  int currentYear = year(currentTime);
+
+  // Formata a hora atual para string
+  String formattedString = String(currentYear) + "-" +
+                           getTwoDigitString(currentMonth) + "-" +
+                           getTwoDigitString(currentDay) + " " +
+                           getTwoDigitString(currentHour) + ":" +
+                           getTwoDigitString(currentMinute) + ":" +
+                           getTwoDigitString(currentSecond) + "";
+  return formattedString;
+}
+
+// Função para formatar um número de um dígito para dois dígitos com zero à esquerda
+String getTwoDigitString(int number) {
+  if (number < 10) {
+    return "0" + String(number);
+  } else {
+    return String(number);
+  }
 }
