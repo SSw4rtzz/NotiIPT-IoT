5990c39193f6ea504b510ee0e6bb233049ee33ca
Sw4rtz <4aronswartz@gmail.com>
2024-03-27 19:30:03 +0000

    Criada página de gestão

---
+++ Front-End/notiipt/src/pages/Gestao.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Gestao() {
+    return (
+        <div>
+            <h1>Página de getão da sala</h1>
+            {/* Add your content here */}
+        </div>
+    );
+}
+
+export default Gestao;
