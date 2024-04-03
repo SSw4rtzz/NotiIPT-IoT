50da9173980437ee9339c1bf00fe3d839dec0b78
Sw4rtz <4aronswartz@gmail.com>
2024-04-03 12:53:04 +0100

    Init página de gestão
    Changelog:
    
    - Criada página de gestão da sala (Criação apenas)
    - Instalado package de rotas react-router-dom

--- Front-End/notiipt/src/App.jsx
+++ Front-End/notiipt/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+import Gestao from './pages/Gestao';
+
 
 function App() {
   const [count, setCount] = useState(0)
