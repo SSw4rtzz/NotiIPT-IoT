162ce59ab9274126ea4762065d3c88cfc1426344
Sw4rtz <4aronswartz@gmail.com>
2024-04-09 19:28:32 +0100

    Sidebar Visual
    Changelog:
    
    - Terminado visual da Sidebar
    - Adicionada imagem de perfil de desenvolvimento
    - Mudança quase completa dos conteúdos CSS da sidebar
    - Adicionada página Lorem para fins de testes
    - Adicionado alias para a font/root do projeto, ajudando assim na navegação de conteúdos pelo projeto
    
    Issues:
    - Quando a sidebar está no estado "fechada" o hover e o active background-color não fica simétrico
    - Classe active deverá passar a ser dinâmica
    - Nome de utilizador e imagem deverá passar a ser dinâmico, quando a API estiver concluída
    - Não gosto muito do estado da seta superior toggle da sidebar

--- Front-End/notiipt/src/App.jsx
+++ Front-End/notiipt/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from './components/side-adminbar/side-adminbar.jsx';
+
+// Importa páginas
 import TestPage from './pages/test.jsx';
+import LoremPage from './pages/lorem.jsx';
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <h1>App</h1>
       <Routes>
         <Route path="/teste" element={<TestPage />} />
+        <Route path="/lorem" element={<LoremPage />} />
       </Routes>
     </div>
     );
--- Front-End/notiipt/src/components/side-adminbar/side-adminbar.jsx
+++ Front-End/notiipt/src/components/side-adminbar/side-adminbar.jsx
@@ -1,28 +1,102 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './side-adminbar.css';
 
+import imagemPerfil from '@/assets/images/profile/perfil.jpg';
+
+// Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faHouse, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
+
 const Sidebar = () => {
+
+  // Esconde e mostra o menu
+  useEffect(() => {
+    const toggle = document.querySelector(".toggle");
+    const toggleClickHandler = () => {
+      const sidebar = document.querySelector('nav');
+      sidebar.classList.toggle("fechada");
+    };
+    if (toggle) {
+      toggle.addEventListener("click", toggleClickHandler);
+    }
+    return () => {
+      if (toggle) {
+        toggle.removeEventListener("click", toggleClickHandler);
+      }
+    };
+  }, []);
+
   return (
-    <nav className="sidebar">
-      <h2>NotiIPT - Admin</h2>
-      <ul>
-        <li>
-          <Link to="/teste">Teste</Link>
-        </li>
-        <li>
-          <Link to="/">Lorem</Link>
-        </li>
-        <li>
-          <Link>Ispu</Link>
-        </li>
-        <li>
-          <Link>Yesii</Link>
-        </li>
-      </ul>
-      </nav>
+    <><nav className="sidebar fechada">
+      <header>
+        <div className="imagem-menu">
+          <span className="imagemPerfil">
+          <img src={imagemPerfil} alt="Perfil" />
+          </span>
+          <div className="text perfil-text">
+            <span className="nome">Admin</span>
+            <span className="funcao">Admin Menu</span>
+          </div>
+        </div>
+  
+        <i className='toggle'>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </i>
+      </header>
+
+      <div className="menu-bar">
+        <div className="menu">
+
+          <ul className="menu-links">
+            <li className="nav-link active">
+              <a href="#">
+               <i className='icon'><FontAwesomeIcon icon={faHouse} /></i>
+                <span className="text nav-text">Inicio</span>
+              </a>
+            </li>
+
+            <li className="nav-link">
+              <a href="#">
+                <i className='icon'></i>
+                <span className="text nav-text">Teste</span>
+              </a>
+            </li>
+
+            <li className="nav-link">
+              <a href="#">
+                <i className='icon'></i>
+                <span className="text nav-text">Lorem</span>
+              </a>
+            </li>
+
+            <li className="nav-link">
+              <i className='icon'></i>
+              <a href="#">
+                <span className="text nav-text">Yesii</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="bottom-content">
+          <li className="">
+            <a href="#">
+            <i className='icon'>
+              <FontAwesomeIcon icon={faRightFromBracket} />
+            </i>
+              <span className="text nav-text">Sair</span>
+            </a>
+          </li>
+        </div>
+      </div>
+    </nav>
+    
+    <section className="home">
+        <div className="text">NotiIPT - Admin</div>
+      </section>
+      </>      
   );
 }
 
