ebd6fd4b7dbf057ce68264aa39b0998895c2f419
Sw4rtz <4aronswartz@gmail.com>
2024-05-05 20:52:24 +0100

    Correção de Issues + Luz
    Changelog:
    - Correção da Issue em que não conseguia adquirir os dados passados da API
    - Aplicado estilo e dados dinâmicos de comparação entre a penúltima medição dos sensores e a última, mostrando a distância positiva ou negativa entre uma e outra no que diz respeito à temperatura e à humidade
    - Colocada barra de luminosidade a funcionar, esta agora será preenchida à medida da percentagem da luminosidade lida pelo sensor, de 0% a 100%
    - Adicionadas duas barrinhas à barra de luminosidade que representa o comando que liga ou desliga a luz, está apontado para os 30%, claro apenas em termos de frontend ainda.
    - Icon da luz deverá ficar agora amarelo quando esta se encontra acesa, conforme as indicações da API

--- Front-End/notiipt/src/pages/GestaoSala.jsx
+++ Front-End/notiipt/src/pages/GestaoSala.jsx
@@ -3,38 +3,52 @@ import React, { useState, useEffect } from 'react';
 
 // Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTemperatureThreeQuarters, faDroplet, faLightbulb, faTemperatureArrowUp, faTemperatureArrowDown} from '@fortawesome/free-solid-svg-icons'; // Solid
+import { faDroplet,
+    faLightbulb,
+    faTemperatureThreeQuarters, faTemperatureArrowUp, faTemperatureArrowDown,
+    faArrowUp, faArrowDown,
+    faGripLinesVertical
+} from '@fortawesome/free-solid-svg-icons'; // Solid
 import {faSun} from '@fortawesome/free-regular-svg-icons'; // Regular
 
 function Gestao() {
-
     const [temperatura, setTemperatura] = useState(''); // Temperatura da sala
-    const [temperaturaAnt, setTemperaturaAnt] = useState(''); // Temperatura da sala
+    const [temperaturaAnterior, setTemperaturaAnterior] = useState(''); // Penúltima medida da temperatura da sala
     const [humidade, setHumidade] = useState(''); // Humidade da sala
+    const [humidadeAnterior, setHumidadeAnterior] = useState(''); // Penúltima medida da humidade da sala
     const [luminosidade, setLuminosidade] = useState(''); // Luminosidade da sala
+    const [luz, setLuz] = useState(''); // Estado da luz da sala
+
 
     const fetchValues = async () => {
         try {
             const response = await fetch('https://66366efb415f4e1a5e275e9a.mockapi.io/dados'); // ! API de Testes, substituir pela API do Projeto
             const data = await response.json();
 
-            // Obtém a temperatura atual e a anterior da API
-            const temperaturaAtual = data[0].temperatura;
-            const temperaturaAnterior = data[1].temperatura;
-
-            // Calcula a diferença entre a temperatura atual e a anterior
-            const diferencaTemperatura = temperaturaAtual - temperaturaAnterior;
-
             // Define a temperatura atual, a anterior e a diferença no estado
-            setTemperatura(temperaturaAtual + 'ºC');
-            setTemperaturaAnt(temperaturaAnterior + 'ºC');
+            setTemperatura(data[0].temperatura);
+            setTemperaturaAnterior(data[1].temperatura);
             
-            setHumidade(data[0].humidade + '%');
-            setLuminosidade(data[0].luminosidade + '%');
+            setHumidade(data[0].humidade);
+            setHumidadeAnterior(data[1].humidade);
+
+            setLuminosidade(data[0].luminosidade);
+
+            setLuz(data[0].luz);
         } catch (error) {
             console.error('Erro:', error);
         }
     };
+
+    let diferencaTemp = 0;
+    let diferencaHum = 0;
+
+    if(temperaturaAnterior !== '') {
+        diferencaTemp = temperatura-temperaturaAnterior;
+    }
+    if(temperaturaAnterior !== '') {
+        diferencaHum = humidade-humidadeAnterior;
+    }
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -55,17 +69,22 @@ function Gestao() {
                 <div className="card-GS-container">
                     <div className="card-GS-container-text">
                         <span className="card-GS-titulo">Temperatura</span>
-                        <h4 className="card-GS-dados">{temperatura}</h4> {/* Mostra temperatura da sala */}
+                        <h4 className="card-GS-dados">{temperatura + 'ºC'}</h4> {/* Mostra temperatura da sala */}
                     </div>
                     <i className='card-GS-icon'><FontAwesomeIcon icon={faTemperatureThreeQuarters} /></i>
                 </div>
                 <div className="card-GS-container-evolucao">
-                    <div className='card-GS-evolucao tempPositivo'>
+                    { temperatura >= temperaturaAnterior ? (
+                        <div className='card-GS-evolucao tempPositivo'>
                         <FontAwesomeIcon icon={faTemperatureArrowUp}/>
-                        5ºC
-                        
-                        <span className='card-GS-evolucao-text'>mais alto</span>
-                    </div>
+                        {diferencaTemp + 'ºC'}
+                        </div>
+                     ) : (
+                        <div className='card-GS-evolucao tempNegativo'>
+                        <FontAwesomeIcon icon={faTemperatureArrowDown}/>
+                        {diferencaTemp + 'ºC'}
+                        </div>
+                    )}
                 </div> {/* Mostra a evolução da temperatura da sala */}
             </div>
 
@@ -73,34 +92,67 @@ function Gestao() {
                 <div className="card-GS-container">
                     <div className="card-GS-container-text">
                         <span className="card-GS-titulo">Humidade</span>
-                        <h4 className="card-GS-dados">{humidade}</h4> {/* Mostra luminosidade da sala */}
+                        <h4 className="card-GS-dados">{humidade + '%'}</h4> {/* Mostra luminosidade da sala */}
                     </div>
                     <i className='card-GS-icon'><FontAwesomeIcon icon={faDroplet} /></i>
                 </div>
+                <div className="card-GS-container-evolucao">
+                    { humidade >= humidadeAnterior ? (
+                        <div className='card-GS-evolucao positivo'>
+                        <FontAwesomeIcon icon={faArrowUp}/>
+                        {diferencaHum + '%'}
+                        </div>
+                     ) : (
+                        <div className='card-GS-evolucao negativo'>
+                        <FontAwesomeIcon icon={faArrowDown}/>
+                        {diferencaHum + '%'}
+                        </div>
+                    )}
+                </div> {/* Mostra a evolução da temperatura da sala */}
             </div>
-
             
             <div className='card-GS'>
                 <div className="card-GS-container">
                     <div className="card-GS-container-text">
                         <span className="card-GS-titulo">Luminusidade</span>
-                        <h4 className="card-GS-dados">{luminosidade}</h4> {/* Mostra estado da luz da sala */}
+                        <h4 className="card-GS-dados">{luminosidade + '%'}</h4> {/* Mostra estado da luz da sala */}
                     </div>
                     <i className='card-GS-icon'><FontAwesomeIcon icon={faSun} /></i>
                 </div>
-                <span className="card-GS-barra"></span> {/* TODO Barra de luminosidade */}
-            </div>
+                <div className="card-GS-container-evolucao">
 
+                <i className="card-GS-lampada"><FontAwesomeIcon icon={faGripLinesVertical} /></i>
+
+                    <div className="card-GS-barra">
+                        
+                        <div className="card-GS-barra-preenchida" style={{ width: `${luminosidade}%` }}>
+                        </div>
+                    </div> {/* TODO Barra de luminosidade */}
+                </div>
+            </div>
+            {luz == 'true' ? (
             <div className='card-GS'>
-                <div className="card-GS-container">
+                <div className="card-GS-container"> 
                     <div className="card-GS-container-text">
                         <span className="card-GS-titulo">Luz</span>
-                        <h4 className="card-GS-dados">Ligada</h4> {/* Mostra estado da luz da sala */}
+                        <h4 className="card-GS-dados">Acesa</h4> {/* Mostra estado da luz da sala */}
                     </div>
-                    <i className='card-GS-icon'><FontAwesomeIcon icon={faLightbulb} /></i>
+                    <i className='card-GS-icon'><FontAwesomeIcon icon={faLightbulb}  style={{color: "#F6F193"}} /></i>
                 </div>
-                <span className="card-GS-btn">Acender</span>
-            </div>
+                <span className="card-GS-btn">Apagar</span>
+                </div>
+                ) : (
+                <div className='card-GS'>
+                    <div className="card-GS-container"> 
+                        <div className="card-GS-container-text">
+                            <span className="card-GS-titulo">Luz</span>
+                            <h4 className="card-GS-dados">Apagada</h4> {/* Mostra estado da luz da sala */}
+                        </div>
+                        <i className='card-GS-icon'><FontAwesomeIcon icon={faLightbulb}/></i>
+                    </div>
+                    <span className="card-GS-btn">Acender</span>
+                </div>
+            )}
         </section>
         </div>
     );
